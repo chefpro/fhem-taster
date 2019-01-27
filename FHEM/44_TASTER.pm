90b86e2a5a11ed2311e3b3b26af857efb87ca637
Peter Rustler <peter@rustlerit.de>
2019-01-27 15:00:17 +0100

    Fix long press and double click for some Switches.

--- FHEM/44_TASTER.pm
+++ FHEM/44_TASTER.pm
@@ -294,7 +294,7 @@ sub Longpress($) {
   my $doubleClick = ReadingsVal($name,"DoubleClick","");
 
   #***** Der Taster wird gerade gedrückt *****#
-  if ($VALUE eq "on") {
+  if ($VALUE eq "on" || $VALUE eq "ON") {
     readingsSingleUpdate($hash,'zeit-down',$start,0);
     if ($doubleClick eq "wait") {
       #ich warte auf den zweiten click und der Taster wurde tatsächlich nochmal gedrückt
